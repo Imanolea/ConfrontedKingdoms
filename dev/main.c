64cf83e14b195de60b4870df1dde59d7db33d1fc
Imanolea <imanoleas@gmail.com>
2015-07-24 20:27:04 +0200

    Sprite del héroe en pantalla

--- dev/main.c
+++ dev/main.c
@@ -1,9 +1,45 @@
 #include <gb/gb.h>
-#include <gb/drawing.h>
+
+extern const unsigned char tileset[];
+
+void init() {
+	
+	wait_vbl_done();
+	disable_interrupts();
+	
+	SPRITES_8x8;
+	
+	DISPLAY_OFF;
+	HIDE_BKG;
+	HIDE_WIN;
+	HIDE_SPRITES;
+	
+	set_sprite_data(0, 4, tileset);
+	set_sprite_tile(0, 1);
+	move_sprite(0, 80, 72);
+	
+	SHOW_BKG;
+	SHOW_WIN;
+	SHOW_SPRITES;
+	DISPLAY_ON;
+	enable_interrupts();
+}
+
+void game() {
+	
+	while (1) {
+		
+		// Lógica del juego
+		
+		wait_vbl_done();
+		
+		// Pintado
+	}
+}
 
 void main() {
-	gotogxy(1, 1);
-	gprintf("Kingdom");
+	init();
 	
-	while(1);
+	game();
 }
+
--- make.bat
+++ make.bat
@@ -1,2 +1,6 @@
-lcc dev/main.c -o rom/Kingdom.gb
+lcc -c -o tileset.o res/tileset.c
+lcc -c -o main.o dev/main.c
+lcc -o rom/Kingdom.gb main.o tileset.o
+del *.o 
+bgb.exe rom/Kingdom.gb
 pause
